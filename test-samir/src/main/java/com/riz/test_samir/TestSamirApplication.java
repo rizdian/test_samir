d6d29c707645089526c604ede3a0e274b929854f
Rizdian <rizdian.dinata@gmail.com>
2025-05-09 19:57:07 +0700

    init

---
+++ test-samir/src/main/java/com/riz/test_samir/TestSamirApplication.java
@@ -0,0 +1,13 @@
+package com.riz.test_samir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSamirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSamirApplication.class, args);
+	}
+
+}
---
+++ test-samir/src/test/java/com/riz/test_samir/TestSamirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riz.test_samir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSamirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
